18cda1c1c27d2e9312d08b1eec443b95c30673cb
XinnYuee-banana <2143866215@qq.com>
2026-02-25 17:24:01 +0800

    orderDetail

--- admin/src/pages/Order/Detail.tsx
+++ admin/src/pages/Order/Detail.tsx
@@ -1,18 +1,17 @@
-import React from 'react'; 
-import { Card, Descriptions } from 'antd'; 
-import { useParams } from 'react-router-dom'; 
- 
-const OrderDetail: React.FC = () => { 
-  const { id } = useParams(); 
-  return ( 
-    <Card title={`订单详情 - ${id}`}> 
-      <Descriptions bordered> 
-        <Descriptions.Item label="联系电话">13800138000</Descriptions.Item> 
-        <Descriptions.Item label="入住日期">2024-01-01</Descriptions.Item> 
-        <Descriptions.Item label="离店日期">2024-01-03</Descriptions.Item> 
-        <Descriptions.Item label="订单金额">¥1299</Descriptions.Item> 
-      </Descriptions> 
-    </Card> 
-  ); 
-} 
-export default OrderDetail; 
+import React from 'react'
+import { Card } from 'antd'
+import { useParams } from 'react-router-dom'
+
+const OrderDetail: React.FC = () => {
+  const { id } = useParams()
+  
+  return (
+    <Card title={`订单详情 - 订单号: ${id}`}>
+      <div style={{ padding: '20px', textAlign: 'center', color: '#999' }}>
+        订单详情开发中...
+      </div>
+    </Card>
+  )
+}
+
+export default OrderDetail
